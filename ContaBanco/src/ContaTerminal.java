3338c4211354eb85c52982ec345e22182e595936
Vince <herosnxds@gmail.com>
2024-08-22 19:27:35 -0300

    conta de banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite seu nome completo");
+        String nome = scanner.nextLine();
+
+        System.out.println("agora, numero de sua agencia");
+        int Agencia = scanner.nextInt();
+
+        System.out.println("inserir valor de deposito inicial");
+        double saldo = scanner.nextDouble();
+
+        String numero = "067-8";
+
+
+        System.out.println("Ola senhor "+ nome +", obrigado por criar uma conta em nosso banco, sua agencia é "+
+         Agencia +", conta "+ numero +" e seu saldo de "+ saldo+" reais ja esta disponivel para saque");
+        }
 }
